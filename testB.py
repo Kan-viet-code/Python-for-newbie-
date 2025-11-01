7da3c83219c73327e403c379be5f02dae3261e97
Kan-viet-code <hieu020207@gmail.com>
2025-11-01 13:55:00 +0700

    Simple register
    Blank

---
+++ testB.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+name = input("Ten nguoi dung:")
+print("Xin Chao " + name.title().strip() + "!")
+print(f"{name.strip().title()} xin hay nhap mat khau!")
+mk = input("Nhap mat khau tai day:")
+print(f"{mk.upper()} day la mat khau cua ban!")
